1fc3ecf52226aab44e433ced9ac313dab51a1225
Hone <hone4292@gmail.com>
2017-11-01 15:12:19 +0200

    valuse changed from html to text

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,8 @@ var rand = function (length) {
 
 $(document).ready(function () {
 
-  // $("#quote").html("<div>Welcome to the Random Quote Generator</div>");
-  // $("#author").html("<div>We hope you will like it</div>");
+  $("#quote").text("Welcome to the Random Quote Generator");
+  $("#author").text("We hope you will like it");
 
   $("#getQuote").on("click", function () {
     $.getJSON("/json/quotes.json", function (json) {
@@ -20,15 +20,15 @@ $(document).ready(function () {
         return (val.id === res);
       });
       json.forEach(function (val) {
-        htmlq += "<div>" + val.quote + "</div>"
-        htmla += "<div>" + val.author + "</div>"
+        htmlq += val.quote
+        htmla += val.author
 
         data += "<a href=" + '"https://twitter.com/share?ref_src=twsrc%5Etfw"' + "class="+ '"twitter-share-button"' +  "data-text=" + '"' + val.quote + " by " + val.author + '"' + "data-show-count=" + '"false"'+ "> Tweet</a >"
         data += "<script async src=" + '"' + "https://platform.twitter.com/widgets.js" + '"' + "charset=" + '"' + "utf-8" + '"' + "></script>"
       });
-      alert(data);
-      $("#quote").html(htmlq);
-      $("#author").html(htmla);
+      
+      $("#quote").text(htmlq);
+      $("#author").text(htmla);
       $("#tweet").html(data);
 
     });
